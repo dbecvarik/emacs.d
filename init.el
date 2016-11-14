a58fd30dced0e278ceb4ce83c981fee7d1957f86
David Becvarik <ldj@l-d-j.org>
2016-11-14 13:07:54 +0100

    made magit to work in fullscreen and restore windows layout after

--- init.el
+++ init.el
@@ -61,4 +61,17 @@
 (use-package elpy
   :init (elpy-enable))
 
-(use-package magit)
+(use-package magit
+  :bind (("C-c g" . magit-status))
+  :config
+  ;; full screen magit-status
+  (defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+  (defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen)))
