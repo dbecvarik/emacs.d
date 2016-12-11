0d0359888b98c40c13c935a3ea97ab387d0ef127
David Becvarik <ldj@l-d-j.org>
2016-12-11 17:26:35 +0100

    added async execution to code block in org mode
    its mapped to C-c c

--- init.el
+++ init.el
@@ -64,6 +64,98 @@
 (setq org-todo-keywords
   '((sequence "NEXT(n)" "TODO(t)" "WAIT(w)" "SOMEDAY(s)" "PROJECT(p)" "|" "DONE(d)" "CANCELED(c)")))
 
+(add-hook 'org-mode-hook 'turn-on-flyspell)
+
+(org-babel-do-load-languages 'org-babel-load-languages
+'((shell . t)))
+
+(require 'org-id)
+
+(require 'ob-sh)
+
+
+(defun org-babel-async-execute:sh ()
+  "Execute the shell src-block at point asynchronously.
+:var headers are supported.
+:results output is all that is supported for output.
+
+A new window will pop up showing you the output as it appears,
+and the output in that window will be put in the RESULTS section
+of the code block."
+  (interactive)
+  (let* ((current-file (buffer-file-name))
+         (uuid (org-id-uuid))
+         (code (org-element-property :value (org-element-context)))
+         (temporary-file-directory ".")
+         (tempfile (make-temp-file "sh-"))
+         (pbuffer (format "*%s*" uuid))
+         (varcmds (org-babel-variable-assignments:sh
+                   (nth 2 (org-babel-get-src-block-info))))
+         process)
+
+    ;; get rid of old results, and put a place-holder for the new results to
+    ;; come.
+    (org-babel-remove-result)
+
+    (save-excursion
+      (re-search-forward "#\\+END_SRC")
+      (insert (format
+               "\n\n#+RESULTS: %s\n: %s"
+               (or (org-element-property :name (org-element-context))
+                   "")
+               uuid)))
+
+    ;; open the results buffer to see the results in.
+    (switch-to-buffer-other-window pbuffer)
+
+    ;; Create temp file containing the code.
+    (with-temp-file tempfile
+      ;; if there are :var headers insert them.
+      (dolist (cmd varcmds)
+        (insert cmd)
+        (insert "\n"))
+      (insert code))
+
+    ;; run the code
+    (setq process (start-process
+                   uuid
+                   pbuffer
+                   "bash"
+                   tempfile))
+
+    ;; when the process is done, run this code to put the results in the
+    ;; org-mode buffer.
+    (set-process-sentinel
+     process
+     `(lambda (process event)
+        (save-window-excursion
+          (save-excursion
+            (save-restriction
+              (with-current-buffer (find-file-noselect ,current-file)
+                (goto-char (point-min))
+                (re-search-forward ,uuid)
+                (beginning-of-line)
+                (kill-line)
+                (insert
+                 (mapconcat
+                  (lambda (x)
+                    (format ": %s" x))
+                  (butlast (split-string
+                            (with-current-buffer
+                                ,pbuffer
+                              (buffer-string))
+                            "\n"))
+                  "\n"))))))
+        ;; delete the results buffer then delete the tempfile.
+        ;; finally, delete the process.
+        (when (get-buffer ,pbuffer)
+          (kill-buffer ,pbuffer)
+          (delete-window))
+        (delete-file ,tempfile)
+        (delete-process process)))))
+
+(add-hook 'org-mode-hook (lambda () (local-set-key (kbd "C-c c") 'org-babel-async-execute:sh)))
+
 (use-package epresent)
 
 (use-package which-key
