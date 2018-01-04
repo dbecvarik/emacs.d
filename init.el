e65e3a30d8544fe96a834a7dddcf4d9c523cb8ea
David Becvarik <dbecvari@redhat.com>
2018-01-04 15:51:12 +0100

    updated all drones

--- init.el
+++ init.el
@@ -119,20 +119,9 @@
          ("C-x M-g" . magit-dispatch-popup))
   :config
   (magit-add-section-hook 'magit-status-sections-hook
-                          'magit-insert-modules-unpulled-from-upstream
-                          'magit-insert-unpulled-from-upstream)
-  (magit-add-section-hook 'magit-status-sections-hook
-                          'magit-insert-modules-unpulled-from-pushremote
-                          'magit-insert-unpulled-from-upstream)
-  (magit-add-section-hook 'magit-status-sections-hook
-                          'magit-insert-modules-unpushed-to-upstream
-                          'magit-insert-unpulled-from-upstream)
-  (magit-add-section-hook 'magit-status-sections-hook
-                          'magit-insert-modules-unpushed-to-pushremote
-                          'magit-insert-unpulled-from-upstream)
-  (magit-add-section-hook 'magit-status-sections-hook
-                          'magit-insert-submodules
-                          'magit-insert-unpulled-from-upstream)
+                          'magit-insert-modules
+                          'magit-insert-stashes
+                          'append)
   ;; full screen magit-status
   (defadvice magit-status (around magit-fullscreen activate)
     (window-configuration-to-register :magit-fullscreen)
@@ -177,7 +166,6 @@
 (use-package tramp
   :defer t
   :config
-  (add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:%h:"))
   (add-to-list 'tramp-default-proxies-alist '("localhost" nil nil))
   (add-to-list 'tramp-default-proxies-alist
                (list (regexp-quote (system-name)) nil nil)))
@@ -202,8 +190,11 @@
                                elpy-module-yasnippet)))
 
 
+(use-package org)
+(use-package org-tempo)
+
 (use-package counsel-projectile
-  :init (counsel-projectile-on))
+  :config (setq counsel-projectile-mode t))
 
 (use-package projectile
   :init (projectile-mode)
